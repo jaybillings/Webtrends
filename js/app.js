ac690d224526c74a4e2313fcea8421c1578eb52b
Jessica Billings <jessica@meteorsolutions.com>
2014-02-10 23:26:57 -0800

    Linted

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 var gallery = angular.module('galleryApp', ['ngRoute']);
 
-gallery.config(['$routeProvider', '$httpProvider',
-    function ($routeProvider, $httpProvider) {
+gallery.config(['$routeProvider',
+    function ($routeProvider) {
         $routeProvider.
             when('/:name', {
                 templateUrl: 'views/photo-browser.html',
@@ -11,75 +11,75 @@ gallery.config(['$routeProvider', '$httpProvider',
                 templateUrl: 'views/error.html'
             }).
             otherwise({
-                redirectTo: '/slaveofosiris',
+                redirectTo: '/slaveofosiris'
             });
     }
 ]);
 
-gallery.controller('galleryCtrl', ['$scope', '$routeParams', '$http', '$timeout', '$cacheFactory',
-    function ($scope, $routeParams, $http, $timeout, $cacheFactory) {
+gallery.controller('galleryCtrl', ['$scope', '$routeParams', '$http', '$timeout',
+    function ($scope, $routeParams, $http, $timeout) {
         var x2js = new X2JS(),
-            api_key = '0e8707c7e42ae98b7a0d4c75f7dc5ef5';
+            api_key = '0e8707c7e42ae98b7a0d4c75f7dc5ef5',
+            parseXML =
+                function (data) {
+                    return x2js.xml_str2json(data.toString());
+                },
+            getPhotos =
+                function (page, count) {
+                    // Get photo IDs
+                    var pageParam = page || 1,
+                        countParam = count || 10;
+                    $http.get('http://api.flickr.com/services/rest/', {
+                        cache: true,
+                        params: {
+                            method: 'flickr.people.getPublicPhotos',
+                            api_key: api_key,
+                            user_id: $scope.user.id,
+                            per_page: countParam,
+                            page: pageParam
+                        }
+                    }).success(function (data) {
+                        var images = parseXML(data).rsp.photos.photo,
+                            i;
+                        $scope.imageList = [];
+                        for (i = 0; i < images.length; i++) {
+                            $scope.imageList.push(images[i]._id);
+                        }
+                    })
+                    .then(function () {
+                        var i, id;
+                        $scope.photos = [];
+                        for (i = 0; i < $scope.imageList.length; i++) {
+                            id = $scope.imageList[i];
+                            $http.get('http://api.flickr.com/services/rest/', {
+                                cache: true,
+                                params: {
+                                    method: 'flickr.photos.getSizes',
+                                    api_key: api_key,
+                                    photo_id: $scope.imageList[i]
+                                }
+                            }).success(function (data, status, headers, config) {
+                                var sizes = parseXML(data).rsp.sizes.size;
+                                $scope.photos.push({
+                                    'thumbnail': sizes[2]._source,
+                                    'display': sizes[5]._source,
+                                    'id': config.params.photo_id
+                                });
+                            });
+                        }
+                    })
+                };
         $scope.user = {name: $routeParams.name, id: ''};
         $scope.imageList = [];
         $scope.photos = [];
         $scope.curPage = 1;
         $scope.currentPhoto = {};
-
-        /* Helper functions */
-        var parseXML = function (data) {
-            return x2js.xml_str2json(data.toString());
-        };
-
-        var getPhotos = function (page, count) {
-            // Get photo IDs
-            var page = page || 1;
-            var count = count || 10;
-            $http.get('http://api.flickr.com/services/rest/', {
-                cache: true,
-                params: {
-                    method: 'flickr.people.getPublicPhotos',
-                    api_key: api_key,
-                    user_id: $scope.user.id,
-                    per_page: count,
-                    page: page
-                }
-            }).success(function (data) {
-                var images = parseXML(data).rsp.photos.photo;
-                $scope.imageList = [];
-                for (var i = 0; i < images.length; i++) {
-                    $scope.imageList.push(images[i]._id);
-                }
-            })
-            .then(function (result) {
-                var id;
-                $scope.photos = [];
-                for (var i = 0; i < $scope.imageList.length; i++) {
-                    id = $scope.imageList[i];
-                    $http.get('http://api.flickr.com/services/rest/', {
-                        cache: true,
-                        params: {
-                            method: 'flickr.photos.getSizes',
-                            api_key: api_key,
-                            photo_id: $scope.imageList[i]
-                        }
-                    }).success(function (data, status, headers, config) {
-                        var sizes = parseXML(data).rsp.sizes.size;
-                        $scope.photos.push({    
-                            'thumbnail': sizes[2]._source,
-                            'display': sizes[5]._source,
-                            'id': config.params.photo_id
-                        });
-                    });
-                }
-            })
-        };
         
         $scope.fetchPics = function (reverse) {
             if (reverse) {
                 $scope.curPage--;
             } else {
-                $scope.curPage++;
+                $scope.curPag++;
             }
             getPhotos($scope.curPage);
         };
@@ -94,10 +94,11 @@ gallery.controller('galleryCtrl', ['$scope', '$routeParams', '$http', '$timeout'
                 }
             })
             .success(function (data) {
-                var picInfo = parseXML(data).rsp.photo;
-                var tags = [];
+                var picInfo = parseXML(data).rsp.photo,
+                    tags = [],
+                    i;
                 if (picInfo && picInfo.tags) {
-                    for (var i = 0; i < picInfo.tags.tag.length; i++) {
+                    for (i = 0; i < picInfo.tags.tag.length; i++) {
                         tags.push(picInfo.tags.tag[i]._raw);
                     }
                 } else {
@@ -109,7 +110,7 @@ gallery.controller('galleryCtrl', ['$scope', '$routeParams', '$http', '$timeout'
                     url: url
                 };
             })
-        }
+        };
 
         /* Engine code */
 
@@ -124,17 +125,13 @@ gallery.controller('galleryCtrl', ['$scope', '$routeParams', '$http', '$timeout'
         .success(function (data) {
             $scope.user.id = parseXML(data).rsp.user._nsid;
         })
-        .then(function (result) {
+        .then(function () {
             var getInfo = function () {
                 if (!$scope.photos[0]) {
                     $timeout(function () {
                         getInfo();
                     }, 1000);
                 } else {
-                    // Put the ids in
-                    /*for (i = 0; i < $scope.imageList.length; i++) {
-                        angular.extend($scope.photos[i], {id: $scope.imageList[i]});
-                    }*/
                     $scope.setCurrentPic($scope.photos[0].id, $scope.photos[0].display);
                 }
             };
@@ -143,7 +140,3 @@ gallery.controller('galleryCtrl', ['$scope', '$routeParams', '$http', '$timeout'
         });
     }
 ]);
-
-
-
-
